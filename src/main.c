fa5c13584242cd3c42c0d14c6e389de7a334286c
Andrew Steel <dev@andrewsteel.net>
2019-01-14 01:09:37 +0000

    Add POSIX curses to project with draw_rect()
      A function to draw a rectangle to a curses window.
    
      Used to display the initial state of the paddles and ball after the
      curses window is initialized.

--- src/main.c
+++ src/main.c
@@ -20,7 +20,37 @@
  * along with this file. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
+#include <curses.h>
+
+/*
+ * Draws a rectangle to a curses window, using half-height characters to double
+ * the vertical resolution.
+ */
+void draw_rect (WINDOW *window, int rect_y, int rect_x, int rect_height, int rect_width)
+{
+	#define HEIGHT_RATIO 2
+
+	for (int x_index = 0; x_index < rect_width; x_index++) {
+		for (int y_index = 0; y_index < rect_height; y_index++) {
+
+			wmove(window, (rect_y + y_index) / HEIGHT_RATIO, rect_x + x_index);
+
+			if ((rect_y + y_index) == 0 || ! ((rect_y + y_index) % HEIGHT_RATIO)) {
+				if (rect_height - y_index >= HEIGHT_RATIO) {
+					waddch(window, ':'); /* full */
+					y_index++;
+				}
+				else {
+					waddch(window, '`'); /* top-half */
+				}
+			}
+			else {
+				waddch(window, ','); /* bottom-half */
+			}
+
+		}
+	}
+}
 
 int main (void)
 {
@@ -45,16 +75,44 @@ int main (void)
 	int score_1 = 0;
 	int score_2 = 0;
 
-	printf("%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %f\n%s: %i\n%s: %i\n",
-		"ball_coord_x", ball_coord_x,
-		"ball_coord_y", ball_coord_y,
-		"ball_speed_x", ball_speed_x,
-		"ball_speed_y", ball_speed_y,
-		"paddle_l_coord_x", paddle_l_coord_x,
-		"paddle_l_coord_y", paddle_l_coord_y,
-		"paddle_r_coord_x", paddle_r_coord_x,
-		"paddle_r_coord_y", paddle_r_coord_y,
-		"score_1", score_1,
-		"score_2", score_2
+	/*
+	 * Initialize curses.
+	 */
+	initscr();
+	noecho();
+	curs_set(0);
+
+	/*
+	 * Create window to represent playing field.
+	 */
+	WINDOW *field = newwin(FIELD_HEIGHT, FIELD_WIDTH, 0, 0);
+
+	/*
+	 * Draw paddles and ball to playing field window.
+	 */
+	draw_rect(field,
+		(int) paddle_l_coord_y,
+		(int) paddle_l_coord_x,
+		PADDLE_HEIGHT,
+		PADDLE_WIDTH
 	);
+	draw_rect(field,
+		(int) paddle_r_coord_y,
+		(int) paddle_r_coord_x,
+		PADDLE_HEIGHT,
+		PADDLE_WIDTH
+	);
+	draw_rect(field,
+		(int) ball_coord_y,
+		(int) ball_coord_x,
+		BALL_HEIGHT,
+		BALL_WIDTH
+	);
+
+	/*
+	 * Render playing field window, loop until SIGINT, then terminate curses.
+	 */
+	wrefresh(field);
+	while (1) {}
+	endwin();
 }
