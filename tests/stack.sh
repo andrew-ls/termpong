360ee90aac8add4037b84028dc4e6fe9f2684662
Andrew Steel <dev@andrewsteel.net>
2019-03-30 21:44:53 +0000

    Adds `tests` directory for unit tests, moving `stack_test` function from `src/lib/stack.c` into `tests/stack.c` with accompanying script.

---
+++ tests/stack.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# This file is part of termpong.
+#
+# Copyright (c) 2019 Andrew Steel <copyright@andrewsteel.net>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# The GNU General Public License Version 3 only:
+#
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License Version 3
+# as published by the Free Software Foundation.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file. If not, see <https://www.gnu.org/licenses/>.
+
+ROOT="$(cd "$(dirname "${0}")"; pwd)/.."
+OBJECT="${ROOT}/tests/stack.o"
+
+if \
+	command c99 \
+		-o "${OBJECT}" \
+		-I "${ROOT}/src" \
+		"${ROOT}/tests/stack.c" \
+		"${ROOT}/src/lib/stack.c" \
+		;
+then
+	EXPECTED="$(
+		printf '%s\n' \
+			'1' \
+			'3' \
+			'3' \
+			'7'
+	)"
+	OUTPUT="$("${OBJECT}")"
+	rm "${OBJECT}"
+	if test "${OUTPUT}" = "${EXPECTED}"; then
+		printf '%s\n' 'SUCCESS'
+		exit 0
+	else
+		printf '%s\n' 'FAILURE'
+		exit 1
+	fi
+else
+	exit 2
+fi
