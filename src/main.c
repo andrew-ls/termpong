49fe8208e8ce55ecc754919f2e0989aa4b94a7d9
Andrew Steel <dev@andrewsteel.net>
2019-04-09 14:29:14 +0100

    Revert really cool ASCII-art comment bubble

--- src/main.c
+++ src/main.c
@@ -163,12 +163,10 @@ void game_tick (void)
 		return;
 	}
 
-	/*_________________________________________________________________
-	 /                                                                 \
-	 |  Bounce the ball off the walls.                                 |
-	 |  The ball's movement beyond the field is added to the rebound.  |
-	 \__   ____________________________________________________________/
-	    \*/
+	/*
+	 * Bounce the ball off the walls.
+	 * The ball's movement beyond the field is added to the rebound.
+	 */
 	if (Ball_getY(ball) < 0.0) {
 		Ball_translocate(ball, Ball_getX(ball), 0 - Ball_getY(ball));
 		Ball_propel(ball, Ball_getXSpeed(ball), -Ball_getYSpeed(ball));
